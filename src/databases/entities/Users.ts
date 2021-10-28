a670166b28b4ff042b688ba99a97fb5804738e5c
Saad El-Madafri <saad.elmadafri@frichti.co>
2021-10-28 15:48:43 +0200

    changle colmn picture to longtext

--- src/databases/entities/Users.ts
+++ src/databases/entities/Users.ts
@@ -19,7 +19,7 @@ export default class Users {
   @Column("varchar",{name : "name",nullable : true,length : 255})
   name : string | null
 
-  @Column("text")
+  @Column("longtext")
   picture : string | null
 
   @Column("varchar",{name : "email",nullable : true,length : 255})
